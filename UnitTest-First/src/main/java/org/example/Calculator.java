eb5e55760fb3510a8c30ad09a690434762e99311
Văn Hiếu <vanhieu99it@gmail.com>
2024-06-06 15:07:13 +0700

    update

--- UnitTest-First/src/main/java/org/example/Calculator.java
+++ UnitTest-First/src/main/java/org/example/Calculator.java
@@ -8,4 +8,11 @@ public class Calculator {
     public int subtract(int a, int b) {
         return a - b;
     }
+
+    public int divide(int a, int b) {
+        if (b == 0) {
+            throw new ArithmeticException("Cannot divide by zero");
+        }
+        return a / b;
+    }
 }
--- UnitTest-First/src/test/java/CalculatorTest.java
+++ UnitTest-First/src/test/java/CalculatorTest.java
@@ -1,7 +1,7 @@
 import org.example.Calculator;
 import org.junit.jupiter.api.Test;
-
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 public class CalculatorTest {
     @Test
@@ -16,5 +16,13 @@ public class CalculatorTest {
         Calculator calculator = new Calculator();
         int result = calculator.subtract(5,2);
         assertEquals(3, result);
+    }
+
+    @Test
+    public void testDevideByZero(){
+        Calculator calculator = new Calculator();
+        assertThrows(ArithmeticException.class, () -> {
+            calculator.divide(1,0);
+        });
     }
 }
